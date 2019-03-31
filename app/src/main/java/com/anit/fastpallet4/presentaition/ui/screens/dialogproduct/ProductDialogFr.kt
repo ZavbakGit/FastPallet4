0c137f866bc0fdcfc3bf74bdeaaa4401cb7573c9
ZavbakGit <ag@799000.ru>
2019-04-01 00:43:20 +1000

    add Inventory

--- app/src/main/java/com/anit/fastpallet4/presentaition/ui/screens/dialogproduct/ProductDialogFr.kt
+++ app/src/main/java/com/anit/fastpallet4/presentaition/ui/screens/dialogproduct/ProductDialogFr.kt
@@ -6,9 +6,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.EditText
 import com.anit.fastpallet4.R
 import com.anit.fastpallet4.presentaition.ui.screens.creatpallet.pallet.PalletCreatePalletFrScreen
@@ -43,9 +40,6 @@ class ProductDialogFr : DialogFragment() {
     var ed_start: EditText? = null
     var ed_end: EditText? = null
     var ed_coff: EditText? = null
-
-
-
 
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
--- app/src/main/java/com/anit/fastpallet4/presentaition/ui/screens/inventory/InventoryView.kt
+++ app/src/main/java/com/anit/fastpallet4/presentaition/ui/screens/inventory/InventoryView.kt
@@ -1,2 +1,15 @@
-package com.anit.fastpallet4.presentaition.ui.screens.Inventory
+package com.anit.fastpallet4.presentaition.ui.screens.inventory
 
+import com.anit.fastpallet4.presentaition.ui.base.BaseView
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+
+interface InventoryView:BaseView{
+    @StateStrategyType(SkipStrategy::class)
+    fun showDialogProduct(title:String
+                          ,barcode: String?
+                          ,weightStartProduct: Int
+                          ,weightEndProduct: Int
+                          ,weightCoffProduct: Float)
+
+}
